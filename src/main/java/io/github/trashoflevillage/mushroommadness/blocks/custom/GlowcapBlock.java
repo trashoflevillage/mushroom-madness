f916ca2be554b9e1ad301b080b8205502724233b
trashoflevillage <uhhnokthxbye@gmail.com>
2024-11-04 13:42:49 -0500

    Glowcap Spore fixes

--- src/main/java/io/github/trashoflevillage/mushroommadness/blocks/custom/GlowcapBlock.java
+++ src/main/java/io/github/trashoflevillage/mushroommadness/blocks/custom/GlowcapBlock.java
@@ -33,7 +33,31 @@ public class GlowcapBlock extends MushroomPlantBlock  {
     @Override
     protected void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
         super.onEntityCollision(state, world, pos, entity);
-        if (!state.get(LIT)) activate(state, pos, world);
+        activate(state, pos, world);
+    }
+
+    private void activate(BlockState state, BlockPos pos, World world) {
+        lastActivated = world.getTime();
+
+        if (!state.get(LIT)) {
+            world.setBlockState(pos, state.with(LIT, true));
+            Random rng = world.getRandom();
+            for (int i = 0; i < 4; i++) {
+                world.addParticle(
+                        ModParticles.GLOWCAP_SPORE,
+                        pos.getX() + (rng.nextBetween(0, 20) * 0.05f),
+                        pos.getY() + (rng.nextBetween(0, 20) * 0.05f),
+                        pos.getZ() + (rng.nextBetween(0, 20) * 0.05f),
+                        0,
+                        rng.nextBetween(5, 10) * 0.01f,
+                        0
+                );
+            }
+        }
+    }
+
+    private void deactivate(BlockState state, BlockPos pos, ServerWorld world) {
+        world.setBlockState(pos, state.with(LIT, false));
     }
 
     @Nullable
@@ -48,26 +72,6 @@ public class GlowcapBlock extends MushroomPlantBlock  {
     @Override
     protected void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
         if (world.getTime() - lastActivated > 600) deactivate(state, pos, world);
-    }
-
-    private void deactivate(BlockState state, BlockPos pos, ServerWorld world) {
-        world.setBlockState(pos, state.with(LIT, false));
-    }
-
-    private void activate(BlockState state, BlockPos pos, World world) {
-        world.setBlockState(pos, state.with(LIT, true));
-        lastActivated = world.getTime();
-
-        Random rng = world.getRandom();
-        for (int i = 0; i < 4; i++) {
-            world.addParticle(ModParticles.GLOWCAP_SPORE,
-                    pos.getX() + (rng.nextBetween(0, 2) * 0.5f),
-                    pos.getY() + (rng.nextBetween(0, 2) * 0.5f),
-                    pos.getZ() + (rng.nextBetween(0, 2) * 0.5f),
-                    0,
-                    0.1,
-                    0);
-        }
     }
 
     static {
