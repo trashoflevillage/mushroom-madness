59e06fcfee6aabdbb06baa36bd7f27243a9e6778
trashoflevillage <uhhnokthxbye@gmail.com>
2024-11-04 14:59:38 -0500

    Added ambient Glowcap particles

--- src/main/java/io/github/trashoflevillage/mushroommadness/blocks/custom/GlowcapBlock.java
+++ src/main/java/io/github/trashoflevillage/mushroommadness/blocks/custom/GlowcapBlock.java
@@ -9,6 +9,7 @@ import net.minecraft.block.RedstoneTorchBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
@@ -18,6 +19,7 @@ import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Property;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
@@ -47,17 +49,25 @@ public class GlowcapBlock extends MushroomPlantBlock  {
             world.setBlockState(pos, state.with(LIT, true));
             Random rng = world.getRandom();
             for (int i = 0; i < 4; i++) {
-                world.addParticle(
-                        ModParticles.GLOWCAP_SPORE,
-                        pos.getX() + (rng.nextBetween(0, 20) * 0.05f),
-                        pos.getY() + (rng.nextBetween(0, 20) * 0.05f),
-                        pos.getZ() + (rng.nextBetween(0, 20) * 0.05f),
-                        0,
-                        rng.nextBetween(5, 10) * 0.01f,
-                        0
-                );
+                spawnParticle(world, pos, rng);
             }
         }
+    }
+
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        if (random.nextFloat() < 0.25f) spawnParticle(world, pos, random);
+    }
+
+    private void spawnParticle(World world, BlockPos pos, Random rng) {
+        world.addParticle(
+                ModParticles.GLOWCAP_SPORE,
+                pos.getX() + (rng.nextBetween(0, 20) * 0.05f),
+                pos.getY() + (rng.nextBetween(0, 20) * 0.05f),
+                pos.getZ() + (rng.nextBetween(0, 20) * 0.05f),
+                0,
+                rng.nextBetween(5, 10) * 0.01f,
+                0
+        );
     }
 
     private void deactivate(BlockState state, BlockPos pos, ServerWorld world) {
