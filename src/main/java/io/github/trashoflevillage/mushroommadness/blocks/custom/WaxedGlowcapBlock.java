1a5e899998cae84dd87f3bda508b8f9f6c283676
trashoflevillage <uhhnokthxbye@gmail.com>
2024-11-05 13:15:35 -0500

    Waxed Glowcaps can be placed anywhere

--- src/main/java/io/github/trashoflevillage/mushroommadness/blocks/custom/WaxedGlowcapBlock.java
+++ src/main/java/io/github/trashoflevillage/mushroommadness/blocks/custom/WaxedGlowcapBlock.java
@@ -9,6 +9,7 @@ import net.minecraft.block.RedstoneTorchBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
@@ -16,6 +17,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import org.jetbrains.annotations.Nullable;
 
@@ -43,5 +45,12 @@ public class WaxedGlowcapBlock extends MushroomPlantBlock  {
                 rng.nextBetween(5, 10) * 0.01f,
                 0
         );
+    }
+
+    @Override
+    protected boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        BlockPos blockPos = pos.down();
+        BlockState blockState = world.getBlockState(blockPos);
+        return this.canPlantOnTop(blockState, world, blockPos);
     }
 }
