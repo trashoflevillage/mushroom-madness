e90df6bf244b996f6f4edc6c977d798188610e6d
trashoflevillage <uhhnokthxbye@gmail.com>
2024-11-08 16:03:25 -0500

    Mushroom Bow beginnings

---
+++ src/main/java/io/github/trashoflevillage/mushroommadness/items/MushroomBow.java
@@ -0,0 +1,9 @@
+package io.github.trashoflevillage.mushroommadness.items;
+
+import net.minecraft.item.BowItem;
+
+public class MushroomBow extends BowItem {
+    public MushroomBow(Settings settings) {
+        super(settings);
+    }
+}
