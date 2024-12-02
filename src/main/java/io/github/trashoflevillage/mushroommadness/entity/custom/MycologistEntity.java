c9c2c50d1172dded43d5684c454dc4894a678bd1
trashoflevillage <lsmiller24@earlham.edu>
2024-12-02 14:32:01 -0500

    Mycologists shoot Red Mushroom Bows (not the others, that's to do)

--- src/main/java/io/github/trashoflevillage/mushroommadness/entity/custom/MycologistEntity.java
+++ src/main/java/io/github/trashoflevillage/mushroommadness/entity/custom/MycologistEntity.java
@@ -19,6 +19,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.entity.projectile.ProjectileUtil;
 import net.minecraft.entity.raid.RaiderEntity;
+import net.minecraft.item.BowItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -37,6 +38,7 @@ import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 
@@ -70,7 +72,7 @@ public class MycologistEntity extends SpellcastingIllagerEntity implements Range
         super.initGoals();
         this.goalSelector.add(0, new SwimGoal(this));
         this.goalSelector.add(1, new LookAtTargetGoal());
-        this.goalSelector.add(6, new BowAttackGoal<>(this, 0.5, 20, 15.0F));
+        this.goalSelector.add(6, new MushroomBowAttackGoal<>(this, 0.5, 20, 15.0F));
         this.goalSelector.add(6, new ConvertCowGoal());
         this.goalSelector.add(8, new WanderAroundGoal(this, 0.6));
         this.goalSelector.add(9, new LookAtEntityGoal(this, PlayerEntity.class, 3.0F, 1.0F));
@@ -166,7 +168,7 @@ public class MycologistEntity extends SpellcastingIllagerEntity implements Range
 
     @Override
     public void shootAt(LivingEntity target, float pullProgress) {
-        ItemStack itemStack = this.getStackInHand(ProjectileUtil.getHandPossiblyHolding(this, Items.BOW));
+        ItemStack itemStack = this.getStackInHand(ProjectileUtil.getHandPossiblyHolding(this, ModItems.RED_MUSHROOM_BOW));
         ItemStack itemStack2 = this.getProjectileType(itemStack);
         PersistentProjectileEntity persistentProjectileEntity = ProjectileUtil.createArrowProjectile(this, itemStack2, pullProgress, itemStack);
         double d = target.getX() - this.getX();
@@ -266,6 +268,136 @@ public class MycologistEntity extends SpellcastingIllagerEntity implements Range
         @Override
         protected Spell getSpell() {
             return Spell.WOLOLO;
+        }
+    }
+
+
+    public class MushroomBowAttackGoal<T extends MycologistEntity> extends Goal {
+        private final T actor;
+        private final double speed;
+        private int attackInterval;
+        private final float squaredRange;
+        private int cooldown = -1;
+        private int targetSeeingTicker;
+        private boolean movingToLeft;
+        private boolean backward;
+        private int combatTicks = -1;
+
+        public MushroomBowAttackGoal(T actor, double speed, int attackInterval, float range) {
+            this.actor = actor;
+            this.speed = speed;
+            this.attackInterval = attackInterval;
+            this.squaredRange = range * range;
+            this.setControls(EnumSet.of(Goal.Control.MOVE, Goal.Control.LOOK));
+        }
+
+        public void setAttackInterval(int attackInterval) {
+            this.attackInterval = attackInterval;
+        }
+
+        @Override
+        public boolean canStart() {
+            return this.actor.getTarget() == null ? false : this.isHoldingBow();
+        }
+
+        protected boolean isHoldingBow() {
+            return this.actor.isHolding(ModItems.RED_MUSHROOM_BOW);
+        }
+
+        @Override
+        public boolean shouldContinue() {
+            return (this.canStart() || !this.actor.getNavigation().isIdle()) && this.isHoldingBow();
+        }
+
+        @Override
+        public void start() {
+            super.start();
+            this.actor.setAttacking(true);
+        }
+
+        @Override
+        public void stop() {
+            super.stop();
+            this.actor.setAttacking(false);
+            this.targetSeeingTicker = 0;
+            this.cooldown = -1;
+            this.actor.clearActiveItem();
+        }
+
+        @Override
+        public boolean shouldRunEveryTick() {
+            return true;
+        }
+
+        @Override
+        public void tick() {
+            LivingEntity livingEntity = this.actor.getTarget();
+            if (livingEntity != null) {
+                double d = this.actor.squaredDistanceTo(livingEntity.getX(), livingEntity.getY(), livingEntity.getZ());
+                boolean bl = this.actor.getVisibilityCache().canSee(livingEntity);
+                boolean bl2 = this.targetSeeingTicker > 0;
+                if (bl != bl2) {
+                    this.targetSeeingTicker = 0;
+                }
+
+                if (bl) {
+                    this.targetSeeingTicker++;
+                } else {
+                    this.targetSeeingTicker--;
+                }
+
+                if (!(d > (double)this.squaredRange) && this.targetSeeingTicker >= 20) {
+                    this.actor.getNavigation().stop();
+                    this.combatTicks++;
+                } else {
+                    this.actor.getNavigation().startMovingTo(livingEntity, this.speed);
+                    this.combatTicks = -1;
+                }
+
+                if (this.combatTicks >= 20) {
+                    if ((double)this.actor.getRandom().nextFloat() < 0.3) {
+                        this.movingToLeft = !this.movingToLeft;
+                    }
+
+                    if ((double)this.actor.getRandom().nextFloat() < 0.3) {
+                        this.backward = !this.backward;
+                    }
+
+                    this.combatTicks = 0;
+                }
+
+                if (this.combatTicks > -1) {
+                    if (d > (double)(this.squaredRange * 0.75F)) {
+                        this.backward = false;
+                    } else if (d < (double)(this.squaredRange * 0.25F)) {
+                        this.backward = true;
+                    }
+
+                    this.actor.getMoveControl().strafeTo(this.backward ? -0.5F : 0.5F, this.movingToLeft ? 0.5F : -0.5F);
+                    if (this.actor.getControllingVehicle() instanceof MobEntity mobEntity) {
+                        mobEntity.lookAtEntity(livingEntity, 30.0F, 30.0F);
+                    }
+
+                    this.actor.lookAtEntity(livingEntity, 30.0F, 30.0F);
+                } else {
+                    this.actor.getLookControl().lookAt(livingEntity, 30.0F, 30.0F);
+                }
+
+                if (this.actor.isUsingItem()) {
+                    if (!bl && this.targetSeeingTicker < -60) {
+                        this.actor.clearActiveItem();
+                    } else if (bl) {
+                        int i = this.actor.getItemUseTime();
+                        if (i >= 20) {
+                            this.actor.clearActiveItem();
+                            this.actor.shootAt(livingEntity, BowItem.getPullProgress(i));
+                            this.cooldown = this.attackInterval;
+                        }
+                    }
+                } else if (--this.cooldown <= 0 && this.targetSeeingTicker >= -60) {
+                    this.actor.setCurrentHand(ProjectileUtil.getHandPossiblyHolding(this.actor, ModItems.RED_MUSHROOM_BOW));
+                }
+            }
         }
     }
 }
